b1a74407f7f1f52c0b238beb683fff67857ed68a
Han <muddybob@gmail.com>
2026-03-22 11:05:10 -0700

    Updating to use direct item usage.

---
+++ EvolutionPOC/Script/TraitEvolutionHeart.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+namespace Evolution;
+
+public class TraitEvolutionHeart : Trait
+{
+    public override bool CanStack => true;
+
+    public override bool IsTool => true;
+    
+    public override void TrySetHeldAct(ActPlan p)
+    {
+        if (p.pos.HasChara)
+        {
+            p.pos.Charas.ForEach(delegate(Chara c)
+            {
+                (bool canEvolve, string charaEvolve) = c.IsEvolvable(this.owner.Thing);
+                if (canEvolve)
+                {
+                    string properName = EClass.sources.charas.map[charaEvolve].GetName();
+                    p.TrySetAct("evolveChara".lang(properName), delegate
+                    {
+                        Dialog.YesNo("evolutionConfirmation".lang(c.Name, properName), delegate
+                        {
+                            Evolve(c, charaEvolve, this.owner.Thing);
+                        });
+                        return true;
+                    });
+                }
+            });
+        }
+    }
+
+    public static void Evolve(Chara c, string charaEvolve, Thing evolutionHeart)
+    {
+        // Create the evolved character.
+        Chara evolvedChara = CharaGen.Create(charaEvolve);
+
+        // Take all the items from the old character and throw them into the new character's inventory.
+        foreach (Thing posession in c.things)
+        {
+            evolvedChara.AddThing(posession);
+        }
+
+        // Copy original elements and max them against the new form.
+        foreach (KeyValuePair<int, Element> element in c.elements.dict)
+        {
+            Element copiedElement = evolvedChara.elements.GetOrCreateElement(element.Key);
+            copiedElement.vBase = Math.Max(copiedElement.vBase, element.Value.vBase);
+            copiedElement.vLink = Math.Max(copiedElement.vLink, element.Value.vLink);
+            copiedElement.vSource = Math.Max(copiedElement.vSource, element.Value.vSource);
+        }
+
+        // Sync Metadata
+        evolvedChara.bio = c.bio;
+        evolvedChara._hobbies = c._hobbies;
+        evolvedChara._ability = c._ability;
+        evolvedChara._tactics = c._tactics;
+        evolvedChara._job = c._job;
+        evolvedChara._works = c._works;
+        evolvedChara.hp = evolvedChara.MaxHP;
+        evolvedChara.mana.Set(evolvedChara.mana.max);
+        evolvedChara._affinity = c._affinity;
+
+        EClass.pc.currentZone.AddCard(evolvedChara, c.pos);
+        EClass._zone.branch.AddMemeber(evolvedChara);
+        EClass.pc.party.AddMemeber(evolvedChara);
+
+        evolvedChara.PlayEffect("aura_heaven");
+        Msg.Say("evolution_complete".langGame(c.NameSimple, c.Name, evolvedChara.Name));
+
+        // Delete the original character
+        c.homeBranch.BanishMember(c, true);
+        EClass.pc.currentZone.RemoveCard(c);
+        c.Destroy();
+        c.RemoveGlobal();
+        
+        // Delete the thing.
+        Thing toDelete = evolutionHeart.Split(1);
+        EClass.pc.RemoveThing(toDelete);
+    }
+}
